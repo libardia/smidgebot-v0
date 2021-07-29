1808cc7c35222d262cb40175a2d7e6351261b760
Anthony Libardi <libardi42@gmail.com>
2021-07-29 12:37:39 -0500

    ...

--- src/invocation.py
+++ src/invocation.py
@@ -20,7 +20,7 @@ class Invocation():
         self.remtimeEarly = (d, h, m)
         logger.log(f'Early time set to {self.remtimeEarly} and reminder time set to {self.remtime}')
 
-    def __repr__(self):
+    def asstr(self)
         attributes = dir(self)
         res = self.__class__.__name__ + "("
         first = True
@@ -33,7 +33,10 @@ class Invocation():
             else:
                 res += ", "
 
-            res += attr + " = " + str( getattr(self, attr))
+            res += attr + " = " + str(getattr(self, attr))
 
         res += ")"
         return res
+
+    def __repr__(self):
+        return self.asstr()
