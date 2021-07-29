47bc9a122aa312f0b890715d3a440bcaaad5014b
Anthony Libardi <libardi42@gmail.com>
2021-07-29 12:32:53 -0500

    ...

--- src/invocation.py
+++ src/invocation.py
@@ -1,6 +1,25 @@
 import logger
 
-class Invocation():
+class stringMe():
+        def __repr__(self):
+            attributes = dir(self)
+            res = self.__class__.__name__ + "("
+            first = True
+            for attr in attributes:
+                if attr.startswith("__") and attr.endswith("__"):
+                    continue
+
+                if(first):
+                    first = False
+                else:
+                    res += ", "
+
+                res += attr + " = " + str( getattr(self, attr))
+
+            res += ")"
+            return res
+
+class Invocation(stringMe):
     def __init__(self, ctx, logger):
         self.exclude = []
         self.earlyCond = False
@@ -19,9 +38,4 @@ class Invocation():
             m += 60
         self.remtimeEarly = (d, h, m)
         logger.log(f'Early time set to {self.remtimeEarly} and reminder time set to {self.remtime}')
-    
-    def __repr__(self):
-        string = 'Invocation('
-        ', '.join(f'{attr}' for attr in dir(self))
-        string += ')'
-        return string
+
