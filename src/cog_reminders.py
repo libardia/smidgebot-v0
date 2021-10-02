00156f543168ab1176b039fc43a81c8f73ddee99
Anthony Libardi <libardi42@gmail.com>
2021-10-02 15:47:54 -0500

    >:(

--- src/cog_reminders.py
+++ src/cog_reminders.py
@@ -245,7 +245,7 @@ class Reminders(Cog):
 
     @tasks.loop(hours=1)
     async def _heartbeat(self):
-        await log(f'Bot is alive. Deleted {cleanLogs()} old logs.')
+        await log(f'Bot is alive. Deleted {await cleanLogs()} old logs.')
     
 def setup(bot):
     bot.add_cog(Reminders(bot))
